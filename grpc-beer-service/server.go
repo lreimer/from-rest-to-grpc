37add1df04fd53ed91a89b8927228f9b065856fe
M.-Leander Reimer <mario-leander.reimer@qaware.de>
2021-11-09 23:13:16 +0100

    Added logging.

--- grpc-beer-service/server.go
+++ grpc-beer-service/server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"log"
 
 	pb "github.com/lreimer/from-rest-to-grpc/grpc-beer-service/proto"
 	"google.golang.org/grpc/codes"
@@ -15,11 +16,13 @@ type Server struct {
 
 // Get the list of all beers
 func (s *Server) AllBeers(ctx context.Context, r *emptypb.Empty) (*pb.GetBeersResponse, error) {
+	log.Println("Getting list of all beers")
 	return &pb.GetBeersResponse{Beers: allBeers()}, nil
 }
 
 // Get a single beer by Asin
 func (s *Server) GetBeer(ctx context.Context, r *pb.GetBeerRequest) (*pb.GetBeerResponse, error) {
+	log.Printf("Getting beer by ASIN %v", r.Asin)
 	beer, found := getBeer(r.Asin)
 	if found {
 		return &pb.GetBeerResponse{Beer: beer}, nil
@@ -30,6 +33,7 @@ func (s *Server) GetBeer(ctx context.Context, r *pb.GetBeerRequest) (*pb.GetBeer
 
 // Create a new beer
 func (s *Server) CreateBeer(ctx context.Context, r *pb.CreateBeerRequest) (*emptypb.Empty, error) {
+	log.Printf("Creating beer %v", r.Beer)
 	_, created := createBeer(r.Beer)
 	if created {
 		return &emptypb.Empty{}, nil
@@ -40,6 +44,7 @@ func (s *Server) CreateBeer(ctx context.Context, r *pb.CreateBeerRequest) (*empt
 
 // Update an existing beer
 func (s *Server) UpdateBeer(ctx context.Context, r *pb.UpdateBeerRequest) (*emptypb.Empty, error) {
+	log.Printf("Updating beer %v", r.Beer)
 	updated := updateBeer(r.Asin, r.Beer)
 	if updated {
 		return &emptypb.Empty{}, nil
@@ -50,6 +55,7 @@ func (s *Server) UpdateBeer(ctx context.Context, r *pb.UpdateBeerRequest) (*empt
 
 // Delete an existing beeer
 func (s *Server) DeleteBeer(ctx context.Context, r *pb.DeleteBeerRequest) (*emptypb.Empty, error) {
+	log.Printf("Creating beer by ASIN %v", r.Asin)
 	deleteBeer(r.Asin)
 	return &emptypb.Empty{}, nil
 }
