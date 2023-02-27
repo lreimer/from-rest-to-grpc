21db485a3631d2582948e15425d44169ffefd60a
M.-Leander Reimer <mario-leander.reimer@qaware.de>
2023-02-28 00:21:02 +0100

    Added logging.

--- quarkus-beer-rest/src/main/java/hands/on/grpc/BeerResource.java
+++ quarkus-beer-rest/src/main/java/hands/on/grpc/BeerResource.java
@@ -5,6 +5,9 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriBuilder;
+
+import io.quarkus.logging.Log;
+
 import java.net.URI;
 import java.util.Objects;
 
@@ -17,6 +20,7 @@ public class BeerResource {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Response all() {
+        Log.info("Getting all JSON beers");
         return Response.ok(repository.all()).build();
     }
 
